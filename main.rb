79397f0215768562aa45bef701a8542cb207ed41
onishi.tsutomu <onishi.tsutomu@a-tm.co.jp>
2021-11-12 09:26:51 +0900

    fix bug

--- main.rb
+++ main.rb
@@ -13,19 +13,25 @@ def we_organization_ids(client)
   ids.uniq
 end
 
+def contains_other_organization_members(we_org_ids, members)
+  members.map { |m| m unless we_org_ids.include? m['organization_id'] }.compact
+end
+
 def collect_that_contains_other_organization_member_in_rooms(client, we_org_ids)
   rooms = client.get_rooms()
   rooms.map do |r|
+    sleep 2
     room_id = r["room_id"]
-    members = client.get_members(room_id)
+    members = client.get_members(room_id: room_id)
     other_org_members = contains_other_organization_members(we_org_ids, members)
-    if other_org_members
+    room_info =
       {
         room_id: room_id,
         room_name: r["name"],
         other_org_members: other_org_members
       }
-    end
+    p room_info
+    room_info unless other_org_members.empty?
   end.compact
 end
 
